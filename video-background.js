915ffcbda7f87177d17e510cd507627df55a2723
Konstantinos Megas <nextdoorpanda@gmail.com>
2023-05-19 15:01:01 +0300

    Revert "adjustVideoSize remove querySelector for video"
    This reverts commit 1723b1a6f96570a18d41483cde8bc9114f1cdfe2.

--- video-background.js
+++ video-background.js
@@ -122,7 +122,15 @@
 	function adjustVideoSize() {
 		const size = getVideoSize();
 		const iframe = videoBg.querySelector('iframe');
+		const video = videoBg.querySelector('video');
 
+		// if (iframe) {
+		// 	iframe.style.width = `${size.width}px`;
+		// 	iframe.style.height = `${size.height}px`;
+		// } else if (video) {
+		// 	video.style.width = `${size.width}px`;
+		// 	video.style.height = `${size.height}px`;
+		// }
 		iframe.style.width = `${size.width}px`;
 		iframe.style.height = `${size.height}px`;
 	}
