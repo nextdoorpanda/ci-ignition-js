14fdaf625b8b9f1363fb0b92cfb58c4279c8c58c
Konstantinos Megas <nextdoorpanda@gmail.com>
2023-05-23 17:59:15 +0300

    fix debounce()

--- wc-search.js
+++ wc-search.js
@@ -254,18 +254,16 @@
 	// N milliseconds. If `immediate` is passed, trigger the function on the
 	// leading edge, instead of the trailing.
 	function debounce(func, wait, immediate) {
-		var timeout;
-		return function() {
-			var context = this, args = arguments;
-			var later = function() {
-				timeout = null;
-				if (!immediate) func.apply(context, args);
-			};
-			var callNow = immediate && !timeout;
+		let timeout;
+		return function (...args) {
+			if (immediate && !timeout) {
+				func(...args);
+			}
 			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(context, args);
-		};
+			timeout = setTimeout(function () {
+				func(...args);
+			}, wait);
+		}
 	}
 
 })();
